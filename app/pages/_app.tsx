1ff0daecb19a663a570464c8dbae946c206d57d8
Matt Wood <matt@cratebind.com>
2020-04-27 01:26:40 -0500

    Add sentry

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,33 +1,39 @@
-import { ThemeProvider, GlobalStyles, defaultTheme } from 'minerva-ui'
+import { ThemeProvider, GlobalStyles, defaultTheme } from "minerva-ui"
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
+  dsn: process.env.SENTRY_DSN,
+})
 
 const customTheme = {
   ...defaultTheme,
   Button: {
     ...defaultTheme.Button,
-    borderRadius: '10px',
-    bg: 'primary',
-    color: 'white',
+    borderRadius: "10px",
+    bg: "primary",
+    color: "white",
     border: 0,
-    fontSize: '16px',
-    borderBottomWidth: '2px',
-    borderBottomStyle: 'inset',
-    borderBottomColor: 'primaryDark',
+    fontSize: "16px",
+    borderBottomWidth: "2px",
+    borderBottomStyle: "inset",
+    borderBottomColor: "primaryDark",
     fontWeight: 700,
     _hover: {
-      bg: 'primaryDark',
+      bg: "primaryDark",
     },
     _active: {
-      bg: 'primaryDark',
+      bg: "primaryDark",
     },
   },
   Input: {
     ...defaultTheme.Input,
-    borderRadius: '10px',
+    borderRadius: "10px",
   },
   colors: {
     ...defaultTheme.colors,
-    primary: '#007d75',
-    primaryDark: '#00645e',
+    primary: "#007d75",
+    primaryDark: "#00645e",
   },
 }
 
