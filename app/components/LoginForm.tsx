8bddecf813964b2204eb9849720e5a8cae8805ca
Matt Wood <matt@cratebind.com>
2020-04-28 11:24:39 -0500

    Fix appearance on input

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react"
-import { Button, Input, Flex, Alert } from "minerva-ui"
-import createUser from "app/users/mutations/createUser"
+import React, { useState } from "react";
+import { Button, Input, Flex, Alert } from "minerva-ui";
+import createUser from "app/users/mutations/createUser";
 
 export default function LoginForm() {
-  const [email, setEmail] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [success, setSuccess] = useState("")
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [success, setSuccess] = useState("");
   // const router = useRouter()
 
   // useEffect(() => {
@@ -24,20 +24,22 @@ export default function LoginForm() {
   // }, [])
 
   async function handleSubmit(e) {
-    e.preventDefault()
-    setIsLoading(true)
+    e.preventDefault();
+    setIsLoading(true);
 
     try {
-      const res = await createUser({ data: { email } })
+      const res = await createUser({ data: { email } });
 
       if (process.env.NODE_ENV !== "production") {
         // when developing locally, just go to the token route instead of sending an email
-        window.location.href = res.url
+        window.location.href = res.url;
       }
-      setSuccess("Great, you've just been emailed a link you can use to sign in")
+      setSuccess(
+        "Great, you've just been emailed a link you can use to sign in"
+      );
     } catch (error) {
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
   }
 
@@ -45,17 +47,24 @@ export default function LoginForm() {
     <Flex alignItems="center" flexDirection="column">
       <Flex as="form" maxWidth="600px" onSubmit={handleSubmit} px={2} py={6}>
         <Input
+          // @ts-ignore
+          appearance="none"
           placeholder="Email Address"
           radiusRight={0}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           disabled={isLoading}
         />
-        <Button type="submit" radiusLeft={0} minWidth="80px" isLoading={isLoading}>
+        <Button
+          type="submit"
+          radiusLeft={0}
+          minWidth="80px"
+          isLoading={isLoading}
+        >
           Log In
         </Button>
       </Flex>
       {success && <Alert status="success">{success}</Alert>}
     </Flex>
-  )
+  );
 }
