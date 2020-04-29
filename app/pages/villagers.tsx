f132a2aaa068a5102ec704dc728098488239da57
Matt Wood <matt@cratebind.com>
2020-04-29 16:53:47 -0500

    Change font-display strategy

--- app/pages/villagers.tsx
+++ app/pages/villagers.tsx
@@ -62,7 +62,7 @@ const Home = ({ user: initialUser, ...props }) => {
           font-family: "Baloo";
           src: url("/fonts/Baloo2-Regular.ttf");
           font-weight: bold;
-          font-display: fallback;
+          font-display: auto;
           font-style: normal;
         }
 
@@ -70,7 +70,7 @@ const Home = ({ user: initialUser, ...props }) => {
           font-family: "BalooBold";
           src: url("/fonts/Baloo2-Bold.ttf");
           font-weight: bold;
-          font-display: fallback;
+          font-display: auto;
           font-style: normal;
         }
 
