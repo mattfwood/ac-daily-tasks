a62067b00bcbf7e7816cbe70f06c68c8e0fc1935
Matt Wood <matt@cratebind.com>
2020-04-30 12:51:22 -0500

    Return null from getCurrentUser when no token is found

--- app/users/queries/getCurrentUser.ts
+++ app/users/queries/getCurrentUser.ts
@@ -1,6 +1,8 @@
 import db from 'db';
 
 export default async function getCurrentUser(token: string) {
+  if (!token) return null;
+
   const user = await db.user.findOne({
     where: { token },
     include: { tasks: true },
